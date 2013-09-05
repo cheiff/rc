f020e8e9a9b1fe9af11fb7d7f3954236eade228b
HN2 <root@hn2.octech.es>
2013-09-05 13:02:11 +0200

    Improved installer

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-if [ -f ~/.vimrc ]; then
-  mv ~/.vimrc ~/.vimrc.old
-fi
-if [ -f ~/.screenrc ]; then
-  mv ~/.screenrc ~/.screenrc.old
-fi
-if [ -f ~/.tmux.conf ]; then
-  mv ~/.tmux.conf ~/.tmux.conf.old
-fi
-ln -s ~/rc/.vimrc ~/.vimrc
-ln -s ~/rc/.screenrc ~/.screenrc
-ln -s ~/rc/.tmux.conf ~/.tmux.conf
+
+function backup_file {
+  if [ -f ~/$1 ]; then
+    mv ~/$1 ~/$1.old
+    echo "Backing up $1"
+  fi
+  echo "Installing new $1"
+  ln -s ~/rc/$1 ~/$1
+}
+
+backup_file .vimrc
+backup_file .screenrc
+backup_file .tmux.conf
+
